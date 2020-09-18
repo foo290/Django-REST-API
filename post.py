1f3a3a49d93e193c84dd64458a9640253356d763
Nitin Sharma <ns290670@gmail.com>
2020-09-18 12:02:11 +0530

    user-api-v1.0

--- post.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-post_data = {'name': 'Gladys'}
-response = requests.post('http://127.0.0.1:8000/webhook/reverse-api-request/callback-point/github/', data=post_data)
--- webhooks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-import webhooks.views as webhook_views
-
-urlpatterns = [
-    path('reverse-api-request/callback-point/github/', webhook_views.github_webhook_point),
-]
