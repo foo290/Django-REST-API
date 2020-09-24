c40d2ce2023a93be38649c464236f65e61f36496
Nitin Sharma <ns290670@gmail.com>
2020-09-24 21:35:34 +0530

    api-test-url pattern removed

--- api_response/urls.py
+++ api_response/urls.py
@@ -32,7 +32,6 @@ urlpatterns += [
     path('users/<slug:userid>/', api_views.api_user_data),
     path('', api_views.default_api_response),
     path('users/', api_views.user_api_endpoint),
-    path('testapi/<slug:user>/<slug:operator>/<slug:id>', api_views.test_api)
 ]
 
 print(f'API_URL_patterns --> {urlpatterns}')
